99dbfea1e9189bc66446a1dcbd2a3e3321a8815a
unknown <xvinh.ir@gmail.com>
2020-07-24 08:55:25 +0700

    fixed footer

--- src/page/MatPlanner.js
+++ src/page/MatPlanner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function MatPlanner() {
 	return (
-		<div>
+		<div className="matplan">
 			<div className="blocker"></div>
 			<iframe className="responsive-iframe" src="https://wotv-guide.com/" title="Material Planner" />
 		</div>
