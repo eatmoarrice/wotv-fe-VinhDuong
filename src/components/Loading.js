d1178548a8ccfa4f19d026baed85c9c879afc32c
eatmoarrice <63140320+eatmoarrice@users.noreply.github.com>
2020-08-16 21:02:26 +0700

    first commit

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 export default function Loading() {
 	return (
 		<div className="loading container flex-column align-items-center d-flex justify-content-center text-center text-white allchars mb-5">
-			<img alt="loading" src="img/UI/loading-chocobo.gif" width="80px" />
+			<img alt="loading" src="https://wotv-guide.com/img/UI/loading-chocobo.gif" width="80px" />
 			LOADING...
 		</div>
 	);
